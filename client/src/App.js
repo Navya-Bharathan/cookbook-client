dea17a377b940011ae31d45e50a61e0405846e9a
SimaranM <106579064+SimaranM@users.noreply.github.com>
2022-09-23 15:10:02 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,121 +18,74 @@ function App() {
         console.log(err);
       });
   }, []);
-  return (
+ return (
     <div className="App">
-      
-{
-    entries.map((entry, index) => {
-    console.log(index)
+      <div className="container">
+        <header>
+          <div className='wrapper'>
+            <div className='wrapper_inner'>
+              <div className="wrapper_inner_menu">
+                <h1 className="wrapper_span">Cookbook</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>News Letter</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className="container_wrapper">
+            <div className='wrapper'>
+              {entries.map((entry, index) => {
+                return (<div key={entry.sys.id}>
 
-    return (<div key={entry.sys.id}>
-      <h2>{entry.fields.recipeTitle}</h2>
-      <p>{entry.fields.recipeIntroduction}</p>
-      <img src={entry.fields.recipeImage.sys.url} alt="burger"/>
+                  <div className='recipetitle_main'>
+                    <h2>{entry.fields.recipeTitle}</h2>
+                    <p>{entry.fields.recipeIntroduction}</p>
+                  </div>
 
-      <div className='text'>
-        {entry.fields.instructions.content.map((content) => {
-          if (content.nodeType === "paragraph") {
-            // return <p>{content.content[0].value}</p>;
-            return content.content.map((element) => {
-              return <p> {element.value}</p>
-            });
-          }
-        })}
-      </div>
+                  <div className='main_img_part'>
+                    <div>
+                      <img src={entry.fields.recipeImage.sys.url} alt="burger" />
+                    </div>
+                    <div className='text'>
+                      {entry.fields.instructions.content.map((content) => {
+                        if (content.nodeType === "paragraph") {
+                          return <p>{content.content[0].value}</p>;
+                          // return content.content.map((element) => {
+                          //   return <p> {element.value}</p>
+                          // });
+                        }
+                      })}
+                    </div>
+                  </div>
 
-      <div className="">
-            {entry.fields.ingredients.content.map((content) => {
-                if (content.nodeType === "table") {
-                    return content.content.map((row) => {
-                        return (
+                  <div className="main_table_part">
+                    {entry.fields.ingredients.content.map((content) => {
+                      if (content.nodeType === "table") {
+                        return content.content.map((row) => {
+                          return (
                             <div className='flex'>
-                                {row.content.map((col) => {
-                                    /*console.log(col);*/
-                                    return <p className='font'> {col.content[0].content[0].value}</p>;
-                                })}
+                              {row.content.map((col) => {
+                                /*console.log(col);*/
+                                return <p className='font'> {col.content[0].content[0].value}</p>;
+                              })}
                             </div>
-                        );
-                    });
-                }
-            })}
-
-        </div>
-    
-    </div>);
-  })}
-      <Footer />
-    </div>
+                          );
+                        });
+                      }
+                    })}
+                  </div>
+                </div>);
+              })}
+              <Footer />
+            </div>
+          </div>
+        </main>
+      </div>
+    </div >
   );
 }
 export default App;
-
-
-// -----------------------------------------------------------------------------------------------------
-// function App() {
-//   const [items, setItems] = useState([{}]);
-
-//   useEffect(() => {
-//     axios
-//       .get('http://localhost:3001/posts')
-//       .then((res) => {
-//         console.log(res.data);
-//         setItems(res.data)
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-
-//   }, []);
-
-//   return (
-//     <div className="App">
-
-//     </div>
-//   );
-// }
-
-// export default App;
-// ----------------------------------------------------------------------------------------------------------
-// function App() {
-//   const [entries, setEntries] = useState([]);
-//   const [article, setArticle] = useState([])
-
-//   useEffect(() => {
-//     axios
-//       .get('http://localhost:3001/posts')
-//       .then((response) => {
-//         setEntries(response.data.items);
-//         setArticle(response.data.items)
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <div className="App">
-//         <div className='container'>
-//           <header>
-//             <div className='wrapper'>
-//               <div className='wrapper_inner'>
-//                 <span className='wrapper_span'>Cookbook</span>
-
-//               </div>
-//             </div>
-//           </header>
-//           <main>
-//             <div className='wrapper'>
-//               <Posts posts={entries} articls={article} />
-//             </div>
-//           </main>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
